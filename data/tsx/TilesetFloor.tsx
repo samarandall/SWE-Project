94cae28bfb16b6c2a66c91cd408a22530fc0a513
sam randall <samrandall100@gmail.com>
2023-04-12 17:14:39 -0500

    expanded map

---
+++ data/tsx/TilesetFloor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="TilesetFloor" tilewidth="64" tileheight="64" tilecount="572" columns="22">
+ <image source="../../graphics/TilesetFloor.png" width="1408" height="1668"/>
+</tileset>

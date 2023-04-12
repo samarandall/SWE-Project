367c47b7a45ae303ca4546ed524e2e5a54a14fd3
sam randall <samrandall100@gmail.com>
2023-04-12 16:17:58 -0500

    fixed player and floor collision

--- code/level.py
+++ code/level.py
@@ -9,7 +9,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
-from debug import debug
+#from debug import debug
 from pytmx.util_pygame import load_pygame
 
 class Level:
@@ -40,22 +40,22 @@ class Level:
         """
         for layer in self.tmx_data.visible_layers:
             if hasattr(layer,'data'):
+                print(layer.name)
                 if layer.name == 'Floor':
-                    for x,y,surf in layer.tiles():
+                    '''for x,y,surf in layer.tiles():
                         pos = (x * TILESIZE, y * TILESIZE)
-                        Tile(pos = pos, surf = surf, groups = [self.visible_sprites])
-                
-                if layer.name == 'Buildings':
-                    for x,y,surf in layer.tiles():
-                        pos = (x * TILESIZE, y * TILESIZE)
-                        Tile(pos = pos, surf = surf, groups = [self.visible_sprites, self.obstacle_sprites])
-
-                if layer.name == 'Player':
+                        Tile(pos = pos, surf = surf, sprite_type='not', groups = [self.visible_sprites])'''
+                    pass
+                elif layer.name == 'Player':
                     for x,y,surf in layer.tiles():
                         pos = (x * TILESIZE, y * TILESIZE)
                         self.player = Player(pos, [self.visible_sprites], self.obstacle_sprites)
+                else:
+                    for x,y,surf in layer.tiles():
+                        pos = (x * TILESIZE, y * TILESIZE)
+                        Tile(pos = pos, surf = surf, sprite_type='not', groups = [self.visible_sprites, self.obstacle_sprites])
 
-
+        self.player = Player(pos, [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
         """
@@ -66,7 +66,7 @@ class Level:
         self.visible_sprites.update()
 
         # need to get rid of this later
-        debug(self.player.direction)
+        #debug(self.player.direction)
 
 
 class YSortCameraGroup(pygame.sprite.Group):
@@ -76,11 +76,18 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_width = self.display_surface.get_size()[0] // 2
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
-    
-    def custom_draw(self, player):
-        
+
+        self.floor_surf = pygame.image.load('../data/floor/floor.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
+
+
+    def custom_draw(self, player): 
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        # drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf,floor_offset_pos)
 
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
--- code/tile.py
+++ code/tile.py
@@ -11,13 +11,17 @@ class Tile(pygame.sprite.Sprite):
     this is the TileDraw class which inherets pygame sprite
     """
 
-    def __init__(self, pos, groups, surf):
+    def __init__(self, pos, groups, sprite_type, surf = pygame.Surface((TILESIZE,TILESIZE)) ):
         """
         this function accepts a position and a sprite group, this shows where to place everything
         """
         super().__init__(groups)
-        #self.sprite_type = sprite_type
+        self.sprite_type = sprite_type
+
         self.image = surf
-        self.rect = self.image.get_rect(topleft=pos)
+        if sprite_type == 'object':
+            self.rect = self.image.get_rect(topleft = (pos[0],pos[1] - TILESIZE))
+        else:
+            self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0,-10)
 

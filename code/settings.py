dcdc4aca399efa2a2d7a23a01688df0a893cb264
dallasgere <dallas.gere05@gmail.com>
2023-04-20 21:06:54 -0500

    adding the UI settings for the game

--- code/settings.py
+++ code/settings.py
@@ -8,6 +8,25 @@ HEIGHT = 720
 FPS = 60
 TILESIZE = 64
 
+# ui
+BAR_HEIGHT = 20
+HEALTH_BAR_WIDTH = 200
+ENERGY_BAR_WIDTH = 140
+ITEM_BOX_SIZE = 80
+UI_FONT = "../graphics/font/joystix.ttf"
+UI_FONT_SIZE = 18
+
+# general colors
+WATER_COLOR = "#71ddee"
+UI_BG_COLOR = "#222222"
+UI_BORDER_COLOR = "#111111"
+TEXT_COLOR = "#EEEEEE"
+
+# ui colors
+HEALTH_COLOR = "red"
+ENERGY_COLOR = "blue"
+UI_BORDER_COLOR_ACTIVE = "gold"
+
 # data for weapons that will be used in the player file
 weapon_data = {
     "sword": {

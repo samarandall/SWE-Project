8bd1186985f71e30de7088676db18473e5e4a4ed
dallasgere <dallas.gere05@gmail.com>
2023-04-25 19:49:11 -0500

    adding game over function

--- code/player.py
+++ code/player.py
@@ -300,6 +300,41 @@ class Player(Entity):
         else:
             self.energy = self.stats["energy"]
 
+    def game_over():
+        # creating font object my_font
+        my_font = pygame.font.SysFont("times new roman", 50)
+
+        # creating a text surface on which text
+        # will be drawn
+        game_over_surface = my_font.render(
+            # f"Score: {str(score)} Speed: {str(snake_speed)}", True, red
+            f"Speed: {str(snake_speed)}",
+            True,
+            color,
+        )
+
+        # create a rectangular object for the text
+        # surface object
+        game_over_rect = game_over_surface.get_rect()
+
+        # setting position of the text
+        game_over_rect.midtop = (window_x / 2, window_y / 4)
+
+        # blit will draw the text on screen
+        game_window.blit(game_over_surface, game_over_rect)
+        pygame.display.flip()
+
+        # after 2 seconds we will quit the
+        # program
+        time.sleep(2)
+
+        # deactivating pygame library
+        pygame.quit()
+
+        # quit the program
+        quit()
+
+
     # def update_player_movement(self):
     def update(self):
         """

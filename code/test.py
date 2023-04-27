1f8ea0072925603294d8fc45c902f2d0e7619cf8
sam randall <samrandall100@gmail.com>
2023-04-27 12:11:20 -0500

    deleted unneccecary print

--- code/test.py
+++ code/test.py
@@ -1,40 +1,89 @@
+import unittest
+from unittest.mock import MagicMock
+from player import Player
+
 import pygame, sys
-from pytmx.util_pygame import load_pygame
+from settings import *
+from level import Level
+from enemy import Enemy
+import random
 
+class TestPlayer(unittest.TestCase):
 
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos, surf, groups):
-        super().__init__(groups)
-        self.image = surf
-        self.rect = self.image.get_rect(topleft=pos)
+    def __init__(self, methodName: str = "runTest") -> None:
+        pygame.init()
+        self.screen_width, self.screen_height = WIDTH, HEIGHT
+        self.screen = pygame.display.set_mode(
+            (self.screen_width, self.screen_height), pygame.RESIZABLE
+        )
+        self.clock = pygame.time.Clock()
+        keys = pygame.key.get_pressed()
 
+    
+    def setUp(self):
+        self.groups = [MagicMock(), MagicMock()]
+        self.obstacle_sprites = MagicMock()
+        self.create_attack = MagicMock()
+        self.destroy_attack = MagicMock()
+        self.create_magic = MagicMock()
+        self.player = Player((0, 0), self.groups, self.obstacle_sprites, self.create_attack, self.destroy_attack, self.create_magic)
 
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-tmx_data = load_pygame("../data/tmx/map.tmx")
-sprite_group = pygame.sprite.Group()
+    def test_keyboard_input(self):
+        # test pressing up arrow
+        pygame = MagicMock()
+        pygame.key.get_pressed = MagicMock(return_value={pygame.K_UP: True})
+        self.player.keyboard_input()
+        self.assertEqual(self.player.direction.y, -1)
+        self.assertEqual(self.player.status, "up")
 
-# cycle through all layers
-for layer in tmx_data.visible_layers:
-    # if layer.name in ('Floor', 'Plants and rocks', 'Pipes')
-    if hasattr(layer, "data"):
-        for x, y, surf in layer.tiles():
-            pos = (x * 64, y * 64)
-            Tile(pos=pos, surf=surf, groups=sprite_group)
+        # test pressing down arrow
+        pygame.key.get_pressed = MagicMock(return_value={pygame.K_DOWN: True})
+        self.player.keyboard_input()
+        self.assertEqual(self.player.direction.y, 1)
+        self.assertEqual(self.player.status, "down")
 
-for obj in tmx_data.objects:
-    pos = obj.x, obj.y
-    if obj.type in ("Building", "Vegetation"):
-        Tile(pos=pos, surf=obj.image, groups=sprite_group)
+        # test pressing left arrow
+        pygame.key.get_pressed = MagicMock(return_value={pygame.K_LEFT: True})
+        self.player.keyboard_input()
+        self.assertEqual(self.player.direction.x, -1)
+        self.assertEqual(self.player.status, "left")
 
+        # test pressing right arrow
+        pygame.key.get_pressed = MagicMock(return_value={pygame.K_RIGHT: True})
+        self.player.keyboard_input()
+        self.assertEqual(self.player.direction.x, 1)
+        self.assertEqual(self.player.status, "right")
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        # test pressing space bar
+        pygame.key.get_pressed = MagicMock(return_value={pygame.K_SPACE: True})
+        self.player.create_attack = MagicMock()
+        self.player.sword_attack_sound.play = MagicMock()
+        self.player.keyboard_input()
+        self.assertTrue(self.player.attacking)
+        self.assertIsNotNone(self.player.attack_time)
+        self.player.create_attack.assert_called_once()
+        self.player.sword_attack_sound.play.assert_called_once()
 
-    screen.fill("black")
-    sprite_group.draw(screen)
+        # test pressing left control
+        pygame.key.get_pressed = MagicMock(return_value={pygame.K_LCTRL: True})
+        self.player.create_magic = MagicMock()
+        self.player.keyboard_input()
+        self.assertTrue(self.player.attacking)
+        self.assertIsNotNone(self.player.attack_time)
+        self.player.create_magic.assert_called_once()
 
-    pygame.display.update()
+        # test pressing e
+        pygame.key.get_pressed = MagicMock(return_value={pygame.K_e: True})
+        self.player.can_switch_magic = True
+        self.player.magic_index = 0
+        self.player.magic = list(self.player.magic.keys())[self.player.magic_index]
+        self.player.magic_switch_time = None
+        self.player.keyboard_input()
+        self.assertFalse(self.player.can_switch_magic)
+        self.assertIsNotNone(self.player.magic_switch_time)
+        self.assertEqual(self.player.magic_index, 1)
+        self.assertEqual(self.player.magic, list(self.player.magic.keys())[self.player.magic_index])
+
+if __name__ == "__main__":
+
+    unittest.main()
